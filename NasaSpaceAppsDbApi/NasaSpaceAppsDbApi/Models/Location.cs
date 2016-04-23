a67dc5d5896464241bd43cf6d4361c2b313ab32e
danmuresan <dan91142006@yahoo.com>
2016-04-23 12:17:41 +0300

    Add API db project
    Supports:
    Users
    Location

---
+++ NasaSpaceAppsDbApi/NasaSpaceAppsDbApi/Models/Location.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NasaSpaceAppsDbApi.Models
+{
+    public class Location
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ NasaSpaceAppsDbApi/NasaSpaceAppsDbApi/Models/NasaDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace NasaSpaceAppsDbApi.Models
+{
+    public class NasaDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Location> Locations { get; set; }
+
+        public NasaDbContext() : base("name=NasaDbContext")
+        {
+        }
+    }
+}
