bd9fbf4a9a0596d626575d988a8bec0e0396f947
danmuresan <dan91142006@yahoo.com>
2016-04-23 19:14:57 +0300

    Make query for location

--- NasaSpaceApp/NasaSpaceApp/UI/MapPageView.xaml.cs
+++ NasaSpaceApp/NasaSpaceApp/UI/MapPageView.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using NasaSpaceApp.Managers;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Services.Maps;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Maps;
@@ -25,11 +28,64 @@ namespace NasaSpaceApp.UI
     /// </summary>
     public sealed partial class MapPageView : Page
     {
+        private BreezometerManager m_aqiManager;
+
+        private List<string> SuggestedCities { get; } = new List<string>
+        {
+            "Cluj-Napoca",
+            "Paris",
+            "Stockholm",
+            "Moscow",
+            "Oslo",
+            "London",
+            "Wien",
+            "Washington",
+            "Prague",
+            "Beijing",
+            "Berlin",
+            "Mumbai",
+            "Sankt Petersburg",
+            "Tokyo",
+            "Barcelona",
+            "Madrid",
+            "Instabul",
+            "New York"
+        };
+
         public MapPageView()
         {
             this.InitializeComponent();
             MapControl.ColorScheme = MapColorScheme.Dark;
             MapControl.Style = MapStyle.Aerial3DWithRoads;
+            SuggestedCities.OrderBy(x => x);
+            m_aqiManager = new BreezometerManager();
+        }
+
+        private void AutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            List<string> myList = new List<string>();
+            foreach (string city in SuggestedCities)
+            {
+                if (city.ToLower().Contains(sender.Text.ToLower()))
+                {
+                    myList.Add(city);
+                }
+            }
+
+            sender.ItemsSource = myList;
+        }
+
+        private async void AutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+            string city = sender.Text;
+            MapLocationFinderResult result = await MapLocationFinder.FindLocationsAsync(city, new Geopoint(new BasicGeoposition()));
+            var location = result.Locations.FirstOrDefault();
+
+            if (location != null)
+            {
+                var pos = location.Point.Position;
+                var data = m_aqiManager.GetBreezometerAirQualityIndexAsync(pos.Latitude, pos.Longitude);
+            }
         }
 
         private async void MapPageView_OnLoaded(object sender, RoutedEventArgs e)
